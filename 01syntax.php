7edb5b04e61ba31a2271bac18a533f20944bc38c
unknown <sandjelkovickg@gmail.com>
2022-04-01 18:50:47 +0200

    new commit

---
+++ 01syntax.php
@@ -0,0 +1,13 @@
+<?php
+    //kljucne reci, klase, funckije ne razlikuju velika i mala slova
+    ECHO "Hello World!<br>";
+    echo "Hello World!<br>";
+    EcHo "Hello World!<br>";
+
+
+    //imena promeljivih su osetljiva na velika i mala slova
+    $color="red";
+    echo "My car is " .$color. "<br>";
+    echo "My house is" .$COLOR. "<br>"; //Undefined variable $COLOR
+    echo "My boat is" .$coLOR. "<br>"; #Undefined variable $coLOR
+
