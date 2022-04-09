4efda0b9c448b058e64fbdbc7d84249aca7beb89
unknown <sandjelkovickg@gmail.com>
2022-04-09 14:00:44 +0200

    new commit

---
+++ 15foreach.php
@@ -0,0 +1,14 @@
+<?php
+    $color = array("red", "green", "blue", "yellow");
+
+    foreach($color as $value){
+        echo "$value <br>";
+    }
+
+
+    $age = array("Petar"=>"35", "Ben"=>"37", "Joe"=>"43");
+
+    foreach($age as $x => $val) {
+        echo "$x = $val<br>";
+    }
+?>
