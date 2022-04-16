4b7131b18ba9ddae5cde5f2de33d3b24bc875275
unknown <sandjelkovickg@gmail.com>
2022-04-16 21:35:11 +0200

    new commit

--- PHPHOME/25get/25get.php
+++ PHPHOME/25get/25get.php
@@ -1,3 +1,6 @@
 <?php
-    echo "Study" . $GET['subject'] . " at " . $_GET['web'];
+    echo "Study" . $_GET['subject'] . " at " . $_GET['web'];
+
+    //posle php? u <a href> preuzimamo vrednosti kljuceva
+    //subject= web=
 ?>
--- PHPTair/04input.php
+++ PHPTair/04input.php
@@ -1,8 +1,15 @@
 <?php
+    $ime    = filter_input(INPUT_POST, 'ime', FILTER_SANITIZE_STRING);
+    $indeks = filter_input(INPUT_POST, 'indeks', FILTER_SANITIZE_NUMBER_INT);
+
+    echo $ime . ' ' . $indeks;
+
+    /*
+    Pocetna dva reda su optimizovana od cetiri koda u nastavku. 
+    Filtriranje znaci da se ne prihvata ono sto nije string ili integer (npr. slovo M kod broja)
+
     $ime =    $_POST['ime'];
     $indeks = $_POST['indeks'];
 
     $ime =    filter_var($ime, FILTER_SANITIZE_STRING);
-    $indeks = filter_var($indeks, FILTER_SANITIZE_NUMBER_INT);
-
-    echo $ime . ' ' . $indeks;
+    $indeks = filter_var($indeks, FILTER_SANITIZE_NUMBER_INT);*/
