5739bde8cb668737f7130b8306373327b5aa23cb
unknown <sandjelkovickg@gmail.com>
2022-04-17 11:04:06 +0200

    new commit

---
+++ PHPTair/05foreachWithIndex.php
@@ -0,0 +1,10 @@
+<?php
+
+    $niz = ['Aleksandra', 'Danijela', 'Marija', 'Milena'];
+
+    foreach ($niz as $i => $ime) {
+        echo $ime . '<br>';
+        echo $ime . 'se nalazi na indeksu ' . $i . '<br>';
+    }
+
+
