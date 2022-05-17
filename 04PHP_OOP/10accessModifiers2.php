76a02e3eada1148dd0f218fc55d3acf1128f2153
unknown <sandjelkovickg@gmail.com>
2022-05-17 20:30:06 +0200

    new commit

---
+++ 04PHP_OOP/10accessModifiers2.php
@@ -0,0 +1,23 @@
+<?php
+    class Fruit {
+        public $name;
+        protected $color;
+        private $weigth;
+
+        function set_name($n) {
+            $this->name = $n;
+        }
+        protected function set_color($n) {
+            $this->color = $n;
+        } 
+        private function set_weight($n) {
+            $this->weight = $n;
+        }
+    }
+
+    $mango = new Fruit();
+    $mango->set_name('Mango'); //OK 
+    $mango->set_color('Yellow'); //ERROR
+    $mango->set_weight('300'); //ERROR
+?>
+
