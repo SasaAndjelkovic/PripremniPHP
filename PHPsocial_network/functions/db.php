f342f3a7af38f81dd38a7f2f79a73497f89ad714
unknown <sandjelkovickg@gmail.com>
2022-06-02 14:14:44 +0200

    new commit

---
+++ PHPsocial_network/functions/db.php
@@ -0,0 +1,22 @@
+<?php
+
+use LDAP\Result;
+
+$con = mysqli_connect(hostname: 'localhost', username: 'root', password: '', database: 'social_network');
+
+function escape($string) {
+    global $con;
+    return mysqli_real_escape_string($con, $string);
+}
+
+function query($query) {
+    global $con;
+    return mysqli_query($con, $query);
+}
+
+function confim($result) {
+    global $con;
+    if (!$result) {
+        die("QUERY FAILED" . mysqli_error($con));
+    }
+}
