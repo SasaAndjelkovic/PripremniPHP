48a1e172e68175c604c22b9b675f80e7ba062f0d
unknown <sandjelkovickg@gmail.com>
2022-06-04 16:10:37 +0200

    new commit

--- PHPsocial_network/functions/functions.php
+++ PHPsocial_network/functions/functions.php
@@ -1,5 +1,9 @@
 <?php
 
+    function clean($string) {
+        return htmlentities($string);
+    }
+
     function set_message($message) {
         if(!empty($message)) {
             $_SESSION['message'] = $message;
@@ -12,6 +16,61 @@
         if(isset($_SESSION['message'])) {
             echo $_SESSION['message'];
             unset($_SESSION['message']);
+        }
+    }
+
+    function email_exists($email) {
+        $email = filter_var($email, FILTER_SANITIZE_EMAIL);
+        $query = "SELECT id FROM users WHERE email = '$email'";
+        $result = query($query);
+
+        if($result->num_rows > 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    function user_exists($user) {
+        $user = filter_var($user, FILTER_SANITIZE_STRING);
+        $query = "SELECT id FROM users WHERE username  = '$user'";
+        $result = query($query);
+
+        if($result->num_rows > 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    function validate_user_registration() {
+        $errors = [];
+        if ($_SERVER['REQUEST_METHOD'] == "POST") {
+            $first_name = clean($_POST['first_name']);
+            $last_name = clean($_POST['last_name']);
+            $username = clean($_POST['username']);
+            $email = clean($_POST['username']);
+            $password = clean($_POST['password']);
+            $confirm_password = clean($_POST['confirm_password']);
+
+            if(strlen($first_name) < 3) {
+                $errors[] = "Your First Name cannot be less then 3 characters!";
+            }
+            if(strlen($last_name) < 3) {
+                $errors[] = "Your Last Name cannot be less then 3 characters!";
+            }
+            if(strlen($username) < 3) {
+                $errors[] = "Your Userame cannot be less then 3 characters!";
+            }
+            if(strlen($username) > 20) {
+                $errors[] = "Your Userame cannot be bigger then 20 characters!";
+            }
+            if(email_exists($email)) {
+                $errors[] = "Sorry that Email is alredy taken!";
+            }
+            if(user_exists($username)) {
+                $errors[] = "Sorry that Username is alredy is taken!";
+            }
         }
     }
 
